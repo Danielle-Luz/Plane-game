ba8275626d37802ea25e858219adc2f008d00bd1
Danielle Luz <daniln2209@gmail.com>
2024-12-09 22:41:16 -0300

    feat: created bounday scene

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -6,8 +6,10 @@ const _FLY_SPEED = -300
 func _physics_process(delta: float) -> void:
 	self.velocity.y += self._SPEED * delta
 	
-	if(Input.is_action_just_pressed("ui_up")):
-		self.velocity.y = self._FLY_SPEED
+	self.fly()
 		
 	self.move_and_slide()
 	
+func fly() -> void:
+	if(Input.is_action_just_pressed("ui_up")):
+		self.velocity.y = self._FLY_SPEED
