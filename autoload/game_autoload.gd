966192e021dda51b128b3baad2814efb9fd46aee
Danielle Luz <daniln2209@gmail.com>
2024-12-11 23:09:28 -0300

    feat: added change scene code

--- autoload/game_autoload.gd
+++ autoload/game_autoload.gd
@@ -1,3 +1,11 @@
 extends Node
 
 const SCROLL_SPEED: float = 200.0
+const GAME_SCENE: PackedScene = preload("res://scenes/game/game.tscn")
+
+func change_scene() -> void:
+	var rendered_scene = self.get_tree().current_scene
+	var game_scene = self.GAME_SCENE.instantiate()
+	
+	if(rendered_scene != game_scene):
+		self.get_tree().change_scene_to_packed(self.GAME_SCENE)
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,6 +1,12 @@
 extends Control
 
 @onready var _highscore_value_label: Label = $MarginContainer/HighscoreValueLabel
+const _GAME_SCENE: PackedScene = preload("res://scenes/game/game.tscn")
 
 func _ready() -> void:
 	self._highscore_value_label.text = str(ScoreAutoload.score)
+	
+func _process(delta: float) -> void:
+	var was_space_pressed: bool = Input.is_action_just_pressed("ui_accept")
+	if(was_space_pressed):
+		GameAutoload.change_scene()
