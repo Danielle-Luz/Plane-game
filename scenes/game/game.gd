f98fad9ef523813223d4991fefcceacbaab0b9cd
Danielle Luz <daniln2209@gmail.com>
2024-12-12 01:17:17 -0300

    feat: instatiate end game screen when player loses

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,19 +1,22 @@
 extends Node2D
 
-const _LASER_PIPES = preload("res://scenes/laser pipes/laser_pipes.tscn")
+const _LASER_PIPES_SCENE = preload("res://scenes/laser pipes/laser_pipes.tscn")
+const _GAME_OVER_SCENE = preload("res://scenes/game over/game_over.tscn")
 
 @onready var _timer: Timer = $Timer
 @onready var _plane: CharacterBody2D = $Plane
+@onready var _end_game_canvas_layer: CanvasLayer = $EndGameCanvasLayer
 
 func _ready() -> void:
 	self.spawn_laser_pipes()
 	SignalsAutoload.game_ended.connect(self.stop_spawning)
+	SignalsAutoload.game_ended.connect(self.show_end_game_screen)
 
 func on_timer_timeout() -> void:
 	self.spawn_laser_pipes()
 	
 func spawn_laser_pipes() -> void:
-	var laser_pipes: LaserPipes = _LASER_PIPES.instantiate()
+	var laser_pipes: LaserPipes = _LASER_PIPES_SCENE.instantiate()
 	var x_starter_position = 308
 	
 	laser_pipes.position.x = x_starter_position
@@ -22,3 +25,7 @@ func spawn_laser_pipes() -> void:
 	
 func stop_spawning() -> void:
 	self._timer.stop()
+	
+func show_end_game_screen() -> void:
+	var game_over_scene: Control = self._GAME_OVER_SCENE.instantiate()
+	self._end_game_canvas_layer.add_child(game_over_scene)
